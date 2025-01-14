582ec26a04eb727eb045d6c38b19338d6daa1d7a
yaseerhaja <yaseerhaja@gmail.com>
2025-01-14 02:00:13 +0000

    Deploying to prod from @ yaseerhaja/cus-transaction-app@47065458764c83d35d5370855cc4e8c17adc2eaa 🚀

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const express_1 = __importDefault(require("express"));
 const express_graphql_1 = require("express-graphql");
 const graphql_1 = require("graphql");
 // Load transaction data
-const transactionData = require('./transactions.json'); // Ensure transactions.json matches the defined structure
+const transactionData = require('./transactions.json');
 const app = (0, express_1.default)();
 // Define GraphQL schema
 const typeDefs = (0, graphql_1.buildSchema)(`
@@ -23,7 +23,7 @@ const typeDefs = (0, graphql_1.buildSchema)(`
   }
 
   type Transaction {
-    id: Int!
+    id: ID!
     timestamp: String!
     amount: Float!
     currencyCode: String!
@@ -44,17 +44,29 @@ const resolvers = {
     },
     getAllTransactions: () => {
         if (transactionData && transactionData.days) {
+            transactionData.days.sort((a, b) => new Date(b.id).getTime() - new Date(a.id).getTime());
+            transactionData.days.forEach(day => {
+                day.transactions.sort((a, b) => new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime());
+            });
             return transactionData.days.map((day) => ({
                 id: day.id,
-                transactions: day.transactions.map((transaction) => ({
-                    id: transaction.id,
-                    timestamp: transaction.timestamp,
-                    amount: transaction.amount,
-                    currencyCode: transaction.currencyCode,
-                    currencyRate: transaction.currencyRate || null,
-                    description: transaction.description,
-                    otherParty: transaction.otherParty || null,
-                })),
+                transactions: day.transactions.map((transaction) => {
+                    const data = {
+                        id: transaction.id,
+                        timestamp: transaction.timestamp,
+                        amount: transaction.amount,
+                        currencyCode: transaction.currencyCode,
+                        currencyRate: transaction.currencyRate || null,
+                        description: transaction.description,
+                        otherParty: transaction.otherParty || null,
+                    };
+                    // Convert amount to EUR if necessary
+                    const amountInEur = transaction.currencyCode === 'USD' && transaction.currencyRate
+                        ? transaction.amount / transaction.currencyRate
+                        : transaction.amount;
+                    // Return the transaction with the amount in EUR and currencyCode as "EUR"
+                    return Object.assign(Object.assign({}, data), { amount: amountInEur, currencyCode: 'EUR' });
+                }),
             }));
         }
         else {
@@ -62,21 +74,17 @@ const resolvers = {
         }
     },
     getTransactionById: ({ transactionId, date, }) => {
-        // Find the day with the matching date
         const day = transactionData.days.find((d) => d.id === date);
         if (!day) {
             throw new Error(`No transactions found for the date: ${date}`);
         }
-        // Find the transaction with the matching ID in the selected day
         const transaction = day.transactions.find(t => t.id === transactionId);
         if (!transaction) {
             throw new Error(`Transaction with ID: ${transactionId} not found on date: ${date}`);
         }
-        // Convert amount to EUR if necessary
         const amountInEur = transaction.currencyCode === 'USD' && transaction.currencyRate
             ? transaction.amount / transaction.currencyRate
             : transaction.amount;
-        // Return the transaction with the amount in EUR and currencyCode as "EUR"
         return Object.assign(Object.assign({}, transaction), { amount: amountInEur, currencyCode: 'EUR' });
     },
 };
