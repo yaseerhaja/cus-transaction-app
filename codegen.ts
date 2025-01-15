daf414958ed09de2f344a1035d7ade11e063cc41
yaseerhaja <yaseerhaja@gmail.com>
2025-01-15 19:52:46 +0100

    Proxy config pointing to aws node server

--- codegen.ts
+++ codegen.ts
@@ -3,7 +3,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "http://localhost:8081/graphql", // Node server Endpoint for Graphql codegen
+  schema: "http://13.60.195.142:8081/graphql", // Node server Endpoint for Graphql codegen
   documents: ["src/**/*.ts", "src/**/*.graphql"],
   generates: {
     "src/generated/graphql.ts": {
