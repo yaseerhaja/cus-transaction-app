695bf31971d16701e41db3aa4dcb8aab6486f735
yaseerhaja <yaseerhaja@gmail.com>
2025-01-15 04:10:30 +0100

    Server Removed and s3 github action worksflow

--- codegen.ts
+++ codegen.ts
@@ -3,7 +3,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "http://localhost:8080/cus-transaction-app/graphql",
+  schema: "http://localhost:8081/graphql", // Node server Endpoint for Graphql codegen
   documents: ["src/**/*.ts", "src/**/*.graphql"],
   generates: {
     "src/generated/graphql.ts": {
